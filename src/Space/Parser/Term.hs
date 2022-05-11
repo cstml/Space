e80fcfb8ee7a33414baad6851788ada08588c2ba
cstml <cstmlcodes@gmail.com>
2022-05-11 09:00:14 +0100

    fix: Working Parser

--- src/Space/Parser/Term.hs
+++ src/Space/Parser/Term.hs
@@ -16,19 +16,10 @@ import Text.Megaparsec.Char.Lexer qualified as P
 
 pTerm :: Parser Term
 pTerm =
-  lex_ . P.choice $ fmap  P.try $
-  [ pEmptyTerm ] <> (pTermWithInfer <$>
-                     [ pChar
-                     , pInteger
-                     , pVariable          
-                     , pPushDef
-                     , pPush
-                     , pPopHo
-                     , pPop
-                     ]
-                    )
-
-pTerm' = pTerm 
+  P.try pEmptyTerm
+    <|> ( P.choice . fmap (P.try . pTermWithInfer) $
+            [pChar, pInteger, pPushDef, pPush, pPopHo, pPop, pVariable]
+        )
 
 pLocation :: Parser Location
 pLocation = lex_ $ do
@@ -38,42 +29,39 @@ pLocation = lex_ $ do
   pure . Location $ [a, b]
 
 pEmptyTerm :: Parser Term
-pEmptyTerm = P.choice $ P.try <$> [ normalEmpty ]
-  where
-    normalEmpty = P.char '*' >> return SEmpty    
+pEmptyTerm = lex_ . P.try $ P.char '*' >> return SEmpty
 
+pEmptyTermInfer :: Parser Term
 pEmptyTermInfer = return SEmpty
 
 pVar :: Parser Variable
 pVar = Variable . pure <$> lex_ P.printChar
 
-pImpliedStar :: Parser Term
-pImpliedStar = do
-  P.choice $ P.try <$> [void P.eol, P.eof, space_, void $ pure SEmpty]
-  pure SEmpty
-
 pTermWithInfer :: Parser (Term -> Term) -> Parser Term
 pTermWithInfer p = do
   v <- p
-  P.choice $ fmap P.try [  lex_ pSeparator >> v <$> pTerm'
-                        ,  lex_ pSeparator >> v <$> pEmptyTermInfer
-                        ,  v <$> pEmptyTermInfer
-                        ]
+  P.choice $
+    fmap
+      P.try
+      [ lex_ pSeparator >> v <$> pTerm
+      , lex_ pSeparator >> v <$> pEmptyTermInfer
+      , v <$> pEmptyTermInfer
+      ]
 
 pVariable :: Parser (Term -> Term)
-pVariable = SVariable  <$> lex_ pVar
+pVariable = SVariable <$> lex_ pVar
 
 pChar :: Parser (Term -> Term)
-pChar =  SChar <$> lex_ ( P.char '\'' >> P.letterChar <* P.char '\'')
+pChar = SChar <$> P.between (lex_ $ P.char '\'') (lex_ $ P.char '\'') P.letterChar
 
 pInteger :: Parser (Term -> Term)
 pInteger = SInteger <$> lex_ P.decimal
 
 pPushDef :: Parser (Term -> Term)
-pPushDef =  (`SPush` DLocation) <$> P.between (lex_ $ P.char '[') (lex_ $ P.char ']') pTerm'
+pPushDef = (`SPush` DLocation) <$> P.between (lex_ $ P.char '[') (lex_ $ P.char ']') pTerm
 
 pPush :: Parser (Term -> Term)
-pPush = flip SPush <$> pLocation <*> P.between (lex_ $ P.char '[') (lex_ $ P.char ']') pTerm'
+pPush = flip SPush <$> pLocation <*> P.between (lex_ $ P.char '[') (lex_ $ P.char ']') pTerm
 
 pPopHo :: Parser (Term -> Term)
 pPopHo = flip SPop (Location "Ho") <$> P.between (lex_ $ P.char '<') (lex_ $ P.char '>') pVar
--- test/Test/Space/Parser.hs
+++ test/Test/Space/Parser.hs
@@ -5,6 +5,7 @@ import Data.Map qualified as M
 import Data.Sequence qualified as S
 import Data.String
 import Data.Text qualified as T
+import Data.Void
 import Space
 import Space.Language qualified as L
 import Space.Parser.Term as X
@@ -18,42 +19,52 @@ import Text.Megaparsec.Char.Lexer qualified as P
 ok :: forall a b. a -> Either b a
 ok = pure
 
-tParseTerm :: String -> Maybe L.Term
-tParseTerm = P.parseMaybe pTerm . fromString
+tParseTerm :: String -> Either (P.ParseErrorBundle T.Text Void) L.Term
+tParseTerm = P.parse pTerm "Test Input" . fromString
 
 unit =
   let stdTest r1 t1 r2 t2 = do
         let str = r1 <> ";*"
-        assertEqual str (Just $ t1 SEmpty) (tParseTerm str)
+        assertEqual str (Right $ t1 SEmpty) (tParseTerm str)
 
         let str = r1
-        assertEqual str (Just $ t1 SEmpty) (tParseTerm str)
-        
+        assertEqual str (Right $ t1 SEmpty) (tParseTerm str)
+
         let str = r1 <> ";"
-        assertEqual str (Just $ t1 SEmpty) (tParseTerm str)
+        assertEqual str (Right $ t1 SEmpty) (tParseTerm str)
 
         let str = r1 <> "    ;    *"
-        assertEqual str (Just $ t1 SEmpty) (tParseTerm str)
+        assertEqual str (Right $ t1 SEmpty) (tParseTerm str)
 
         let str = r1 <> " ; " <> r2 <> " ; *"
-        assertEqual str (Just $ t1 $ t2 SEmpty) (tParseTerm str)
+        assertEqual str (Right $ t1 $ t2 SEmpty) (tParseTerm str)
 
         let str = r1 <> " ; " <> r2 <> ";"
-        assertEqual str (Just $ t1 $ t2 SEmpty) (tParseTerm str)
+        assertEqual str (Right $ t1 $ t2 SEmpty) (tParseTerm str)
 
         let str = r1 <> " {-; " <> r2 <> " -}; *"
-        assertEqual str (Just $ t1 SEmpty) (tParseTerm str)
+        assertEqual str (Right $ t1 SEmpty) (tParseTerm str)
    in testGroup
         "Parser Unit Tests"
-        [ testCase "Parse empty Term" $ assertEqual "*" (Just SEmpty) (tParseTerm "*")
-        , testCase "Parse Variable." $ let p = "[x;*];*" in  assertEqual p (Just SEmpty) (tParseTerm p)
-        , testCase "Parse Push Variable." $ stdTest "[x;]" (SVariable (Variable "x")) "y" (SVariable (Variable "y"))
+        [ testCase "Parse empty Term" $ assertEqual "*" (Right SEmpty) (tParseTerm "*")
+        , testCase "Parse Push empty." $
+            let p = "[*];*"
+             in assertEqual p (Right $ SPush SEmpty DLocation SEmpty) (tParseTerm p)
+        , testCase "Parse Push Variable." $
+            let p = "[x;*];*"
+             in assertEqual p (Right $ SPush (SVariable (Variable "x") SEmpty) DLocation SEmpty) (tParseTerm p)
+        , testCase "Parse Push Variable." $
+            stdTest
+              "[x]"
+              (SPush (SVariable (Variable "x") SEmpty) DLocation)
+              "y"
+              (SVariable (Variable "y"))
         , testCase "Parse Char." $ stdTest "'x'" (SChar 'x') "'y'" (SChar 'y')
         , testCase "Parse Int." $ stdTest "1" (SInteger 1) "20" (SInteger 20)
         , let t x = SPush (SVariable (Variable x) SEmpty) DLocation
            in testCase "Parse Push Default Location." $ stdTest "[ x ; * ]" (t "x") "[y;*]" (t "y")
         , let t x = SPush (SVariable (Variable x) SEmpty) DLocation
-          in testCase "Parse Push Default Location." $ stdTest "[ ]" (t "x") "[ ]" (t "y")
+           in testCase "Parse Push Default Location." $ stdTest "[ x ]" (t "x") "[ y ]" (t "y")
         , let t x = SPush (SVariable (Variable x) SEmpty) DLocation
            in testCase "Parse Push Default Locataion." $ stdTest "[ x;* ]" (t "x") "[y;*]" (t "y")
         , let t x = SPush (SVariable (Variable x) SEmpty) DLocation
